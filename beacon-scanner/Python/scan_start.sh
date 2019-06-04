c6e33e2fcb3fc5cd23f61e46fd0d1aa71ed5fe80
mvartani76 <mike.vartanian@gmail.com>
2019-06-03 23:56:21 -0400

    Disabling wlan0 power management in scan_start.sh so it happens every time the code resets. Previously it only did this at 12:20am.

--- beacon-scanner/Python/scan_start.sh
+++ beacon-scanner/Python/scan_start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# disabling wifi power management
+printf "\nDisabling WiFi Power Management...\n"
+/sbin/iwconfig wlan0 power off
+
 # run Beacon Scanner App using provided certificates
 printf "\nRunning Beacon Scanner Application...\n"
 python $AWS_IOT_PYTHON_CMD_OPTIONS
